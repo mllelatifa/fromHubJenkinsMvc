f4e9046d824e10e9d43f20ae27f18456ed5ea1fa
Yanis Adekalom <yanis971.60@gmail.com>
2023-08-04 11:08:28 +0200

    Initial commit

---
+++ src/main/java/fr/dawan/demoJenkinsMvc/DemoJenkinsMvcApplication.java
@@ -0,0 +1,13 @@
+package fr.dawan.demoJenkinsMvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoJenkinsMvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoJenkinsMvcApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fr/dawan/demoJenkinsMvc/DemoJenkinsMvcApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.dawan.demoJenkinsMvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoJenkinsMvcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
